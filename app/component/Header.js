72151070fa52de2bbd583e45ca00cf81c564b512
MashalTech-rgb <mashalpugc@gmail.com>
2024-10-02 07:42:06 -0700

    comit

--- app/component/Header.js
+++ app/component/Header.js
@@ -2,9 +2,9 @@ import React from 'react'
 import Link from 'next/link'
 const Header = () => {
 return (
-    <div className='bg-slate-600 text-white font-bold flex items-center h-10 px-12 gap-8 justify-between'>
+    <div className='navbar'>
       <h2>Logo</h2>
-<div className='flex gap-8'>
+<div className='navbar container'>
 <Link href='/About'>About</Link>
 <Link href='/Services'>Services</Link>
 <Link href='/Contact'>Contact</Link>
